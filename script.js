f28edd0091c014bca20b1ab268bb793ae3b5b008
ferlmdl <131288252+ferlmdl@users.noreply.github.com>
2024-11-04 20:25:22 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('proyecto1').addEventListener('click', function(event) {
+    event.preventDefault();
+    window.location.href = 'https://github.com/ferlmdl/Desarrollo_web';
+});
